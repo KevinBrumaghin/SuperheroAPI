88c422dd8793638558260fde8ca5d960f465edc0
Kevin <139509367+KevinBrumaghin@users.noreply.github.com>
2023-08-30 20:54:36 -0400

    8:54 8/30

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 //HTML Elements//
-const heroName1 = document.querySelector('.hero1');
-const heroStats1 = document.querySelector('.stats1');
-const heroName2 = document.querySelector('.hero2');
-const heroStats2 = document.querySelector('.stats2');
-const input = document.querySelector('.searchValue');
-const searchButton = document.querySelector('.searchBtn');
-const multi = document.querySelector('.multiOptions');
+let heroName1 = document.querySelector('.hero1');
+let heroStats1 = document.querySelector('.stats1');
+let heroName2 = document.querySelector('.hero2');
+let heroStats2 = document.querySelector('.stats2');
+let input = document.querySelector('.searchValue');
+let searchButton = document.querySelector('.searchBtn');
+let multi = document.querySelector('.multiOptions');
 
 //Boolean to know which hero slot to fill
 let isHero1 = false; //Hero slot 1 not full on startup
@@ -18,12 +18,9 @@ let superHeroBio;
 //Superhero API key
 const apiKey = '6444932778877033';
 
-
-
-
 //////////////////////////////////////////////////////////////////////////////////////////////////
 //If multiple options for a hero appear, this helps decide which to show, hero slot 1
-function optionsListener1(option, index, hero){
+function optionsListener1(index, hero){
   superHeroBio = hero.data.results[index].biography;
   superHero = hero.data.results[index].powerstats;
 
@@ -47,8 +44,8 @@ function optionsListener1(option, index, hero){
   isHero1 = true;
 }
 
-//If multiple options for a hero appear, this helps decide which to show, hero slot 2
-function optionsListener2(option, index, hero){
+// //If multiple options for a hero appear, this helps decide which to show, hero slot 2
+function optionsListener2(index, hero){
   superHeroBio = hero.data.results[index].biography;
   superHero = hero.data.results[index].powerstats;
 
@@ -77,14 +74,6 @@ function optionsListener2(option, index, hero){
 }
 //////////////////////////////////////////////////////////////////////////////////////////////////
 
-function titleCase(string) {
-  var sentence = string.toLowerCase().split(" ");
-  for(var i = 0; i< sentence.length; i++){
-     sentence[i] = sentence[i][0].toUpperCase() + sentence[i].slice(1);
-  }
-document.write(sentence.join(" "));
-return sentence;
-}
 
 //Event listener for search button click
 searchButton.addEventListener('click', async function(){
@@ -110,9 +99,9 @@ searchButton.addEventListener('click', async function(){
             searchButton.disabled = true;
             input.disabled = true;
             do{
-              multi.insertAdjacentHTML('beforeend', `<li id = "options" class = "option${i+1}">${hero.data.results[i].biography['full-name']}</li>`)
-              console.log(hero.data.results[i].biography['full-name'])
-              i++;
+                multi.insertAdjacentHTML('beforeend', `<li id = "options" class = "option${i+1}">${hero.data.results[i].biography['full-name']}</li>`)
+                console.log(hero.data.results[i].biography['full-name'])
+                i++;
             }
             while(i<hero.data.results.length);
 
@@ -128,20 +117,21 @@ searchButton.addEventListener('click', async function(){
             let option10 = document.querySelector('.option10');
 
             try{
-            option1.addEventListener('click', async function(event){optionsListener1(option1, 0, hero)})
-            option2.addEventListener('click', async function(event){optionsListener1(option2, 1, hero)})
-            option3.addEventListener('click', async function(event){optionsListener1(option3, 2, hero)})
-            option4.addEventListener('click', async function(event){optionsListener1(option4, 3, hero)})
-            option5.addEventListener('click', async function(event){optionsListener1(option5, 4, hero)})
-            option6.addEventListener('click', async function(event){optionsListener1(option6, 5, hero)})
-            option7.addEventListener('click', async function(event){optionsListener1(option7, 6, hero)})
-            option8.addEventListener('click', async function(event){optionsListener1(option8, 7, hero)})
-            option9.addEventListener('click', async function(event){optionsListener1(option9, 8, hero)})
-            option10.addEventListener('click', async function(event){optionsListener1(option10, 9, hero)})
+            option1.addEventListener('click', async function(event){optionsListener1(0, hero)})
+            option2.addEventListener('click', async function(event){optionsListener1(1, hero)})
+            option3.addEventListener('click', async function(event){optionsListener1(2, hero)})
+            option4.addEventListener('click', async function(event){optionsListener1(3, hero)})
+            option5.addEventListener('click', async function(event){optionsListener1(4, hero)})
+            option6.addEventListener('click', async function(event){optionsListener1(5, hero)})
+            option7.addEventListener('click', async function(event){optionsListener1(6, hero)})
+            option8.addEventListener('click', async function(event){optionsListener1(7, hero)})
+            option9.addEventListener('click', async function(event){optionsListener1(8, hero)})
+            option10.addEventListener('click', async function(event){optionsListener1(9, hero)})
             }
             catch(err){
               console.log('')
             }
+            heroName1.innerText = input.value;
           }
 
           //If not multiple options
@@ -155,11 +145,11 @@ searchButton.addEventListener('click', async function(){
             heroStats1.insertAdjacentHTML('beforeend',`<li>${superHero.power}</li>`)
             heroStats1.insertAdjacentHTML('beforeend',`<li>${superHero.speed}</li>`)
             heroStats1.insertAdjacentHTML('beforeend',`<li>${superHero.strength}</li>`)
+            heroName1.innerText = hero.data.results[0].name;
           }
         isHero1 = true;
         console.log('Data:', superHero);
         input.value = "";
-        heroName1.innerText = hero.data.results[0].name;
       }
 
       //Input hero into hero spot 2!
@@ -171,14 +161,26 @@ searchButton.addEventListener('click', async function(){
           multi.insertAdjacentHTML('beforeend', `<li class = "pickPlease">Please Pick an Option Below:</li>`)
           searchButton.disabled = true;
           input.disabled = true;
+          console.log(document.querySelector('.listName1').innerText)
+          let name1 = document.querySelector('.listName1').innerText;
+          let prevName = '';
           do{
-            multi.insertAdjacentHTML('beforeend', `<li id = "options" class = "option${i+1}">${hero.data.results[i].biography['full-name']}</li>`)
-            console.log(hero.data.results[i].biography['full-name'])
+            let curName = hero.data.results[i].biography['full-name'];
+            let nextName = hero.data.results[i+1].biography['full-name'];
+            if(name1 == curName){
+              console.log("Repeat Name")
+              multi.insertAdjacentHTML('beforeend', `<li id = "options" class = "option${i+1}">${hero.data.results[i+1].biography['full-name']}</li>`)
+            }
+            else if(curName == prevName){
+              multi.insertAdjacentHTML('beforeend', `<li id = "options" class = "option${i+1}">${hero.data.results[i].biography['full-name']}</li>`)
+              console.log(hero.data.results[i].biography['full-name'])
+            }
             i++;
+            prevName = nextName;
           }
-          while(i<hero.data.results.length);
+          while(i<hero.data.results.length-1);
 
-          let option1 = document.querySelector('.option1');
+            let option1 = document.querySelector('.option1');
             let option2 = document.querySelector('.option2');
             let option3 = document.querySelector('.option3');
             let option4 = document.querySelector('.option4');
@@ -190,20 +192,21 @@ searchButton.addEventListener('click', async function(){
             let option10 = document.querySelector('.option10');
 
             try{
-            option1.addEventListener('click', async function(event){optionsListener2(option1, 0, hero)})
-            option2.addEventListener('click', async function(event){optionsListener2(option2, 1, hero)})
-            option3.addEventListener('click', async function(event){optionsListener2(option3, 2, hero)})
-            option4.addEventListener('click', async function(event){optionsListener2(option4, 3, hero)})
-            option5.addEventListener('click', async function(event){optionsListener2(option5, 4, hero)})
-            option6.addEventListener('click', async function(event){optionsListener2(option6, 5, hero)})
-            option7.addEventListener('click', async function(event){optionsListener2(option7, 6, hero)})
-            option8.addEventListener('click', async function(event){optionsListener2(option8, 7, hero)})
-            option9.addEventListener('click', async function(event){optionsListener2(option9, 8, hero)})
-            option10.addEventListener('click', async function(event){optionsListener2(option10, 9, hero)})
+              option1.addEventListener('click', async function(event){optionsListener2(0, hero)})
+              option2.addEventListener('click', async function(event){optionsListener2(1, hero)})
+              option3.addEventListener('click', async function(event){optionsListener2(2, hero)})
+              option4.addEventListener('click', async function(event){optionsListener2(3, hero)})
+              option5.addEventListener('click', async function(event){optionsListener2(4, hero)})
+              option6.addEventListener('click', async function(event){optionsListener2(5, hero)})
+              option7.addEventListener('click', async function(event){optionsListener2(6, hero)})
+              option8.addEventListener('click', async function(event){optionsListener2(7, hero)})
+              option9.addEventListener('click', async function(event){optionsListener2(8, hero)})
+              option10.addEventListener('click', async function(event){optionsListener2(9, hero)})
             }
             catch(err){
               console.log('')
             }
+            heroName2.innerText = input.value;
         }
 
         //If not multiple options
@@ -217,12 +220,11 @@ searchButton.addEventListener('click', async function(){
           heroStats2.insertAdjacentHTML('beforeend',`<li>${superHero.power}</li>`)
           heroStats2.insertAdjacentHTML('beforeend',`<li>${superHero.speed}</li>`)
           heroStats2.insertAdjacentHTML('beforeend',`<li>${superHero.strength}</li>`)
+          heroName2.innerText = hero.data.results[0].name;
         }
         isHero2 = true;
         console.log('Data:', superHero);
         input.value = "";
-        heroName2.innerText = hero.data.results[0].name;
-
         if(isHero2 == true){
           searchButton.disabled = true;
           input.disabled = true;
@@ -278,20 +280,21 @@ input.addEventListener("keypress", async function(event){
           let option10 = document.querySelector('.option10');
 
           try{
-            option1.addEventListener('click', async function(event){optionsListener1(option1, 0, hero)})
-            option2.addEventListener('click', async function(event){optionsListener1(option2, 1, hero)})
-            option3.addEventListener('click', async function(event){optionsListener1(option3, 2, hero)})
-            option4.addEventListener('click', async function(event){optionsListener1(option4, 3, hero)})
-            option5.addEventListener('click', async function(event){optionsListener1(option5, 4, hero)})
-            option6.addEventListener('click', async function(event){optionsListener1(option6, 5, hero)})
-            option7.addEventListener('click', async function(event){optionsListener1(option7, 6, hero)})
-            option8.addEventListener('click', async function(event){optionsListener1(option8, 7, hero)})
-            option9.addEventListener('click', async function(event){optionsListener1(option9, 8, hero)})
-            option10.addEventListener('click', async function(event){optionsListener1(option10, 9, hero)})
+            option1.addEventListener('click', async function(event){optionsListener1(0, hero)})
+            option2.addEventListener('click', async function(event){optionsListener1(1, hero)})
+            option3.addEventListener('click', async function(event){optionsListener1(2, hero)})
+            option4.addEventListener('click', async function(event){optionsListener1(3, hero)})
+            option5.addEventListener('click', async function(event){optionsListener1(4, hero)})
+            option6.addEventListener('click', async function(event){optionsListener1(5, hero)})
+            option7.addEventListener('click', async function(event){optionsListener1(6, hero)})
+            option8.addEventListener('click', async function(event){optionsListener1(7, hero)})
+            option9.addEventListener('click', async function(event){optionsListener1(8, hero)})
+            option10.addEventListener('click', async function(event){optionsListener1(9, hero)})
           }
           catch(err){
             console.log('')
           }
+          heroName1.innerText = input.value;
         }
 
         //If not multiple options
@@ -305,11 +308,11 @@ input.addEventListener("keypress", async function(event){
           heroStats1.insertAdjacentHTML('beforeend',`<li>${superHero.power}</li>`)
           heroStats1.insertAdjacentHTML('beforeend',`<li>${superHero.speed}</li>`)
           heroStats1.insertAdjacentHTML('beforeend',`<li>${superHero.strength}</li>`)
+          heroName1.innerText = hero.data.results[0].name;
         }
         isHero1 = true;
         console.log('Data:', superHero);
         input.value = "";
-        heroName1.innerText = hero.data.results[0].name;
       }
 
       //Input hero into hero spot 2!
@@ -321,12 +324,24 @@ input.addEventListener("keypress", async function(event){
           multi.insertAdjacentHTML('beforeend', `<li id = "options" class = "pickPlease">Please Pick an Option Below:</li>`)
           searchButton.disabled = true;
           input.disabled = true;
+          console.log(document.querySelector('.listName1').innerText)
+          let name1 = document.querySelector('.listName1').innerText;
+          let prevName = '';
           do{
-            multi.insertAdjacentHTML('beforeend', `<li class = "option${i+1}">${hero.data.results[i].biography['full-name']}</li>`)
-            console.log(hero.data.results[i].biography['full-name'])
+            let curName = hero.data.results[i].biography['full-name'];
+            let nextName = hero.data.results[i+1].biography['full-name'];
+            if(name1 == curName){
+              console.log("Repeat Name")
+              multi.insertAdjacentHTML('beforeend', `<li id = "options" class = "option${i+1}">${hero.data.results[i+1].biography['full-name']}</li>`)
+            }
+            else if(curName == prevName){
+              multi.insertAdjacentHTML('beforeend', `<li id = "options" class = "option${i+1}">${hero.data.results[i].biography['full-name']}</li>`)
+              console.log(hero.data.results[i].biography['full-name'])
+            }
             i++;
+            prevName = nextName;
           }
-          while(i<hero.data.results.length);
+          while(i<hero.data.results.length-1);
 
           let option1 = document.querySelector('.option1');
             let option2 = document.querySelector('.option2');
@@ -340,20 +355,21 @@ input.addEventListener("keypress", async function(event){
             let option10 = document.querySelector('.option10');
 
             try{
-            option1.addEventListener('click', async function(event){optionsListener2(option1, 0, hero)})
-            option2.addEventListener('click', async function(event){optionsListener2(option2, 1, hero)})
-            option3.addEventListener('click', async function(event){optionsListener2(option3, 2, hero)})
-            option4.addEventListener('click', async function(event){optionsListener2(option4, 3, hero)})
-            option5.addEventListener('click', async function(event){optionsListener2(option5, 4, hero)})
-            option6.addEventListener('click', async function(event){optionsListener2(option6, 5, hero)})
-            option7.addEventListener('click', async function(event){optionsListener2(option7, 6, hero)})
-            option8.addEventListener('click', async function(event){optionsListener2(option8, 7, hero)})
-            option9.addEventListener('click', async function(event){optionsListener2(option9, 8, hero)})
-            option10.addEventListener('click', async function(event){optionsListener2(option10, 9, hero)})
+            option1.addEventListener('click', async function(event){optionsListener2(0, hero)})
+            option2.addEventListener('click', async function(event){optionsListener2(1, hero)})
+            option3.addEventListener('click', async function(event){optionsListener2(2, hero)})
+            option4.addEventListener('click', async function(event){optionsListener2(3, hero)})
+            option5.addEventListener('click', async function(event){optionsListener2(4, hero)})
+            option6.addEventListener('click', async function(event){optionsListener2(5, hero)})
+            option7.addEventListener('click', async function(event){optionsListener2(6, hero)})
+            option8.addEventListener('click', async function(event){optionsListener2(7, hero)})
+            option9.addEventListener('click', async function(event){optionsListener2(8, hero)})
+            option10.addEventListener('click', async function(event){optionsListener2(9, hero)})
             }
             catch(err){
               console.log('')
             }
+            heroName2.innerText = input.value;
         }
 
         //If not multiple options
@@ -367,12 +383,11 @@ input.addEventListener("keypress", async function(event){
           heroStats2.insertAdjacentHTML('beforeend',`<li>${superHero.power}</li>`)
           heroStats2.insertAdjacentHTML('beforeend',`<li>${superHero.speed}</li>`)
           heroStats2.insertAdjacentHTML('beforeend',`<li>${superHero.strength}</li>`)
+          heroName2.innerText = hero.data.results[0].name;
         }
         isHero2 = true;
         console.log('Data:', superHero);
         input.value = "";
-        heroName2.innerText = hero.data.results[0].name;
-
         if(isHero2 == true){
           searchButton.disabled = true;
           input.disabled = true;
